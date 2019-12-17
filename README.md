28541e73ca182803765c835368bfa26762388e6b
Travis CI <travis@travis-ci.org>
2019-12-17 13:19:52 +0800

    test done,

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # README.md
 
 ### this repo hold the source of portfolio.louislabs.com
+
+### spin me up
+```
+cd <PROJECT ROOT>
+yarn
+```
+
+### deploy to staging
+`yarn deploy_staging`
+
+### deploy to production
+`yarn deploy_production`
+
+### to test
+`yarn test`
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,3 +1,16 @@
-export default () => (
-  <div>Next-Offline Example, try to install app via chrome</div>
-)
+import Head from 'next/head'
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <p>Hello World!</p>
+    </div>
+  )
+}
+
+export default IndexPage
