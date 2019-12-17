10c8cab7d3d83ce0abc4c1ae3fccc3b9e702bbfb
Travis CI <travis@travis-ci.org>
2019-12-17 14:46:08 +0800

    update messaging,

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -2,3 +2,7 @@
 
 firebase deploy -f --project nextjs-projects --only hosting:staging --token $FIREBASE_TOKEN --non-interactive
 firebase deploy -f --project nextjs-projects --only functions:staging --token $FIREBASE_TOKEN --non-interactive
+
+cd $TRAVIS_BUILD_DIR/_ref/travis-broadcast
+pipenv sync
+pipenv run python3 travis-broadcast.py \#travis-build-result "publish to staging plant done"
