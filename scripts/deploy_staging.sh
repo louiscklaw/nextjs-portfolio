81dca338c907b2a50afb84b8027d0ddbe1ed6806
Travis CI <travis@travis-ci.org>
2019-12-17 15:29:31 +0800

    try fix and test,

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+cd $TRAVIS_BUILD_DIR
+
 firebase deploy -f --project nextjs-projects --only hosting:staging --token $FIREBASE_TOKEN --non-interactive
 firebase deploy -f --project nextjs-projects --only functions:staging --token $FIREBASE_TOKEN --non-interactive
 
