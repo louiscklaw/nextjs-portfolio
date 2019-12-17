6c2bcec9d56fa5b515572df36068ce2e9076452f
louiscklaw <louiscklaw@gmail.com>
2019-12-17 22:51:03 +0800

    update for better message,

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+TRAVIS_BUILD_WEB_URL=http://www.example.com
 
 firebase deploy --only hosting:staging --token $FIREBASE_TOKEN --non-interactive
 firebase deploy --only functions:staging_next --token $FIREBASE_TOKEN --non-interactive
@@ -7,3 +7,5 @@ firebase deploy --only functions:staging_next --token $FIREBASE_TOKEN --non-inte
 cd $TRAVIS_BUILD_DIR/_ref/travis-broadcast
 pipenv sync
 pipenv run python3 travis-broadcast.py \#travis-build-result "publish to staging plant done"
+pipenv run python3 travis-broadcast.py \#travis-build-result "build log: $TRAVIS_BUILD_WEB_URL"
+pipenv run python3 travis-broadcast.py \#travis-build-result "hosting url: https://staging-portfolio-nextjs.firebaseapp.com"
