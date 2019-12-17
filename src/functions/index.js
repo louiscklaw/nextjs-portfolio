5c1b655d705a9186b9e2e9e7f22818aad5d088cd
Travis CI <travis@travis-ci.org>
2019-12-17 19:31:35 +0800

    test,

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,19 +3,22 @@ const functions = require('firebase-functions')
 const next = require('next')
 
 
-var dev = process.env.NODE_ENV !== 'production'
 var production_distDir = `${path.relative(process.cwd(), __dirname)}/next`
 var staging_distDir = `${path.relative(process.cwd(), __dirname)}/next-staging`
-var result_distDir = dev ? staging_distDir: production_distDir
 
 // NOTE: the dev: false is related to https://github.com/zeit/next.js/issues/8893
-var app = next({
-  dev: false,
-  conf: { distDir: result_distDir },
-})
-var handle = app.getRequestHandler()
+var production_app = next({ dev: false, conf: { distDir: production_distDir } })
 
-exports.next = functions.https.onRequest((req, res) => {
+var production_handle = production_app.getRequestHandler()
+
+exports.production_next = functions.https.onRequest((req, res) => {
   console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-  return app.prepare().then(() => handle(req, res))
+  return production_app.prepare().then(() => production_handle(req, res))
+})
+
+var staging_app = next({ dev: false, conf: { distDir: staging_distDir } })
+var staging_handle = production_app.getRequestHandler()
+exports.staging_next = functions.https.onRequest((req, res) => {
+  console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
+  return staging_app.prepare().then(() => staging_handle(req, res))
 })
