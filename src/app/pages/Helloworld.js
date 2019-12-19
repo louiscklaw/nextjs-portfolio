a49fa9c97435bb90fa4ad2bbdb5158df7a444e8e
Travis CI <travis@travis-ci.org>
2019-12-19 15:56:58 +0800

    init helloworld page,

---
+++ src/app/pages/Helloworld.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Helloworld extends Component {
+  state = {
+
+  }
+  render(){
+    return(
+      <p>helloworld</p>
+    )
+  }
+}
+
+export default Helloworld
