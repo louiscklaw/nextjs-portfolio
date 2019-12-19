d7c831a1a3329f168285b0a64b3a32795326e4a3
Travis CI <travis@travis-ci.org>
2019-12-19 15:54:29 +0800

    init credits page,

---
+++ src/app/pages/Credits.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Credits extends Component {
+  state = {
+
+  }
+  render(){
+    return(
+      <div>
+        <p>in progress</p>
+
+      </div>
+    )
+  }
+}
+
+export default Credits
