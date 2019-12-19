b86d15a8eeaf6eb8c7f5fadd05e1a2a296176815
Travis CI <travis@travis-ci.org>
2019-12-19 15:24:34 +0800

    init about link,

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import Link from 'next/link'
+
 function IndexPage() {
 
   return (
@@ -9,7 +11,13 @@ function IndexPage() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <p>Hello World! update content</p>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About Us</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
